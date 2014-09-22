8acce7483b6bb55698695d6ebf60bcdb3362510b
karol <kandrusieczko@eyeota.com>
2014-09-22 17:13:51 +0800

    sending only first 100 stat rows to improve performance

--- locust/web.py
+++ locust/web.py
@@ -159,7 +159,7 @@ def request_stats():
             "avg_content_length": s.avg_content_length,
         })
     
-    report = {"stats":stats, "errors":[e.to_dict() for e in runners.locust_runner.errors.itervalues()]}
+    report = {"stats":stats[:100], "errors":[e.to_dict() for e in runners.locust_runner.errors.itervalues()]}
     if stats:
         report["total_rps"] = stats[len(stats)-1]["current_rps"]
         report["fail_ratio"] = runners.locust_runner.stats.aggregated_stats("Total").fail_ratio
